9e6bdd01c30567a4c2924372ebc68749edbcc81f
benel2606 <benel2606@gmail.com>
2018-01-05 17:46:12 +0200

    Json

---
+++ Json.py
@@ -0,0 +1,14 @@
+import json
+"""FetchJson------------------------------------------------------------------------------------------------------------"""
+def FetchJson():
+    jsonFile = open('CVen7db.json', 'r')
+    jsonData = json.load(jsonFile)
+    jsonFile.close()
+    return jsonData
+"""SaveJason------------------------------------------------------------------------------------------------------------"""
+def SaveJason(jsonData):
+    f = open('CVen7db.json', 'w')
+    newJson = json.dumps(jsonData)
+    f.write(newJson)
+    f.close()
+
